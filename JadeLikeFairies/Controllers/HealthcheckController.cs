57ffadc12c5d04d2a57c79fadf2655a3a5027dab
ImoutoChan <kirirrin@gmail.com>
2017-11-30 23:58:36 +0300

    Add healthcheck and remove valuescontroller

---
+++ JadeLikeFairies/Controllers/HealthcheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JadeLikeFairies.Controllers
+{
+    [Route("api/[controller]")]
+    public class HealthcheckController : Controller
+    {
+        // GET api/healthcheck
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
